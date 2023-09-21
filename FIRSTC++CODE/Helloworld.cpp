56f76c91d4aa019c870eab8d218368786a6b5168
WILLSPHINX <wkwabuiya@outlook.com>
2023-09-21 22:59:44 +0300

    first commit

---
+++ FIRSTC++CODE/Helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Hello world. this is myn first C++ program";
+    cout<<"I am very excited and ready to learn C++";
+    cout<<"This is a great University"; 
+}
