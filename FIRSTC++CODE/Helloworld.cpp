7ec3a81006026d4a18c4c5bebb0069bc9970aa93
WILLSPHINX <wkwabuiya@outlook.com>
2023-09-28 18:28:10 +0300

    corrections1

--- FIRSTC++CODE/Helloworld.cpp
+++ FIRSTC++CODE/Helloworld.cpp
@@ -6,4 +6,5 @@ int main()
     cout <<"Hello world. this is myn first C++ program";
     cout<<"I am very excited and ready to learn C++";
     cout<<"This is a great University"; 
+    return 0;
 }
