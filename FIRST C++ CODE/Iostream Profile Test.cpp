497c85468033997c221b359ff3f9edee5f61d350
WILLSPHINX <wkwabuiya@outlook.com>
2023-09-28 23:15:12 +0300

    add on

---
+++ FIRST C++ CODE/Iostream Profile Test.cpp
@@ -0,0 +1,24 @@
+//Simple Iostream Test Program
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+string name;
+int age;
+string city;
+string email;
+cout << "........MY Profile......."<<endl;
+
+cout << "Enter a Name:  ";
+cin>> name;
+cout << "Enter Age:  ";
+cin >> age;
+cout <<"Enter City:  ";
+cin>>city;
+cout << "Enter email address:  ";
+cin>>email;
+
+
+
+}
