8ebae0528ba3657b8143b759cf93d0609a797a55
WILLSPHINX <wkwabuiya@outlook.com>
2023-10-01 14:46:50 +0300

    assignment

---
+++ FIRST C++ CODE/CLASS ASSIGNMENT 1.cpp
@@ -0,0 +1,17 @@
+//Class Assignment 1.
+// Name: William Kagoiyo Wabuiya.
+// Admin No: 105241.
+// Email: wkwabuiya@outlook.com.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Welcome to the Wonderful World of C++ !  ";
+cout << "This is my First C++ output Statement. ";
+cout << "Soon, I will learn how  to input data into my program.";
+}
+
+// End assignment.
+// BBIT 1102.
