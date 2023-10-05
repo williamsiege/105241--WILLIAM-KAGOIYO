bc9b7e2d0f60df296f98296d5dc5d1e4a57cb176
WILLSPHINX <wkwabuiya@outlook.com>
2023-10-05 18:14:50 +0300

    n

--- FIRST C++ CODE/Basic Output&Escape Sequences.cpp
+++ FIRST C++ CODE/Basic Output&Escape Sequences.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 cout << "Strathmore University is the leading" <<endl;
-cout << "university in the region."<< endl;
+cout << "university in the region.";
 
 return 0;
 }
