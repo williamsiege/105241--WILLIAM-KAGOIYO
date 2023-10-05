e59cb8bb2a574def01f231a3c3557cf1b669575b
WILLSPHINX <wkwabuiya@outlook.com>
2023-10-05 16:18:05 +0300

    er

--- FIRST C++ CODE/CLASS ASSIGNMENT 1.cpp
+++ FIRST C++ CODE/CLASS ASSIGNMENT 1.cpp
@@ -10,7 +10,7 @@ int main()
 {
 cout << "Welcome to the Wonderful World of C++ !  ";
 cout << "This is my First C++ output Statement. ";
-cout << "Soon, I will learn how  to input data into my program.";
+cout << "Soon, I will learn how  to input data into my program. ";
 return 0;
 }
 
