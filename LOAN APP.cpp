02c9158bdacc75356409fe022a9bdb868a0252d6
WILLSPHINX <wkwabuiya@outlook.com>
2023-11-03 14:15:31 +0300

    learning

--- LOAN APP.cpp
+++ LOAN APP.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    int age, balance, crb, goodCustomer;
+   long int age, balance, goodCustomer,crb;
     cout << "Please enter the age :  ";
     cin >> age;
     cout<<"\n";
@@ -19,6 +19,33 @@ int main()
     cout << "Please enter Good conduct time :  ";
     cin >> goodCustomer;
     cout<<"\n";
-
+if (age>=22){
+    if (balance>=10000)
+    {
+       if (crb==0 ) {
+        if (goodCustomer>=9)
+            {
+            cout<< "Qualified to apply for Loan"<<endl;
+            cout<< "******DATA GIVEN***** :\n\n";
+            cout<< "Age is : "<<age<<endl;
+            cout<< "Bank Balance is : "<<balance<<endl;
+            cout<< "CRB is : "<<crb<<endl;
+            cout<< "Good Conduct time: "<<goodCustomer<<endl;
+            cout<< "********CONGRATULATIONS********\n\n";
+            }
+        }
+        
+    }
+    
+}else{
+    cout << "Not Qualified for Loan"<<endl;
+    cout<< "******DATA GIVEN***** :\n\n";
+    cout<< "Age is : "<<age<<endl;
+    cout<< "Bank Balance is kSH: "<<balance<<endl;
+    cout<< "CRB is : "<<crb<<endl;
+    cout<< "Good Conduct time: "<<goodCustomer<<"MONTHS"<<endl;
+    cout<< "********SORRY TRY AGAIN LATER********\n\n";   
+       
+}
     
 }
