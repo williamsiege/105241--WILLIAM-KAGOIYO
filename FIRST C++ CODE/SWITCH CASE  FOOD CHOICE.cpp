27c839fd51e38f9dbc3ce41b4d5afa2ef169a839
WILLSPHINX <wkwabuiya@outlook.com>
2023-11-09 23:07:22 +0300

    ASSIGNMENT

--- FIRST C++ CODE/SWITCH CASE  FOOD CHOICE.cpp
+++ FIRST C++ CODE/SWITCH CASE  FOOD CHOICE.cpp
@@ -1,36 +1,43 @@
 /*Writing a C++ program 
-using Switch…Case to determine the maximum number given
-two numbers from a user.*/
+using Switch…Case to determine food choice in a restaraunt.*/
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 int main() {
-    // Introducing variables for the two numbers
-    double num1, num2;
+    int comboChoice;
+    double totalBill = 0.0;
 
-    // Capture two numbers from the user
-    cout << "Enter the first number: "<<endl;
-    cin >> num1;
+    cout << "Restaurant Menu:" <<endl;
+    cout << "1. Combo 1: Burger + Fries + SODA Drink ($15.99)" <<endl;
+    cout << "2. Combo 2: Pizza + Salad + JUICE Drink ($12.99)" <<endl;
+    cout << "3. Combo 3: Sandwich + Soup + WATER Drink ($8.99)" <<endl;
 
-    cout << "Enter the second number: "<<endl;
-    cin >> num2;
+    // Prompt the user to choose a food combo
+    cout << "Enter your choice (1-3): ";
+    cin >> comboChoice;
 
-//Introducing switch case variable
-    int choice;
-
-    // Determine the maximum number using switch-case
-    switch (choice) {
-        // first switch case to decide food Combo
+    // Determining  the selected food items and their costs using a switch case
+    switch (comboChoice) {
         case 1:
-            
+            cout << "Selected Combo: Burger + Fries + SODA Drink" <<endl;
+            totalBill += 10.99;
             break;
         case 2:
-            
+            cout << "Selected Combo: Pizza + Salad +  JUICE Drink" <<endl;
+            totalBill += 12.99;
             break;
+        case 3:
+            cout << "Selected Combo: Sandwich + Soup + WATER Drink" <<endl;
+            totalBill += 8.99;
+            break;
+        default:
+            cout << "Invalid choice. Please enter a valid option (1-3)." <<endl;
+            return 1;
     }
 
+    // Display the total bill
+    cout << "Total Bill: $ "<<totalBill<<endl;
 
     return 0;
 }
-// code tested and debugged.
