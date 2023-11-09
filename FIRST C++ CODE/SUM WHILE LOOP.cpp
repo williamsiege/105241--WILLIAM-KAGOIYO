b0433627886a9bb8ee150c14a3dbf59bf382aef7
WILLSPHINX <wkwabuiya@outlook.com>
2023-11-09 18:37:37 +0300

    iterative structures

--- FIRST C++ CODE/SUM WHILE LOOP.cpp
+++ FIRST C++ CODE/SUM WHILE LOOP.cpp
@@ -6,5 +6,16 @@ using namespace std;
 
 int main()
 {
+// beginning program
+cout << "A program to compute the sum of numbres between 20 and 25;"<<endl;
 
+int value1= 20 , sum = 0;
+while (value1<=25)
+{
+    /* code */ sum = sum + value1;
+    value1++;
+
+}
+cout << " Sum of NUmbers from 20 to 25 is : "<<sum<<endl;
+return 0;
 }
