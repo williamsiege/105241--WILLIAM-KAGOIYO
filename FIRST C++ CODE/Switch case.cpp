63f33e4f758ec7d7e613a749fd38d59b7d4bb2c0
WILLSPHINX <wkwabuiya@outlook.com>
2023-11-16 17:47:08 +0300

    rev

--- FIRST C++ CODE/Switch case.cpp
+++ FIRST C++ CODE/Switch case.cpp
@@ -9,7 +9,7 @@ int main()
 int opt; //Declaring switch variable......
 cout <<"Hi . This is your BBIT Timetable(Sep - DEC 2023)"<<endl;
 cout <<"Please choose between Monday, Tuesday, Wednesday,Thursday, Friday";// telling user to choose a day to view
-cout << "Days are represented from numbers 1 to 5";
+cout << "Days are represented from numbers 1 to 5 (1-5)";
 
 cin >> opt;
 
@@ -22,31 +22,31 @@ case 1:
        cout << "\t1. BBIT1110: Theology"<<endl;
     break;//directs Compiler to stop execution and jump to the next case
 case 2:
-       cout << "Monday Classes\n.................\n" ;
+       cout << "Tuesday Classes\n.................\n" ;
        cout << "\t1. BBIT1012: Analog electronics 2"<<endl;
        cout << "\t2. BBIT1107: Fundamentals of Business 2"<<endl;
-       cout << "\t1. BBIT1110: Theology"<<endl;
+       cout << "\t. BBIT1110: Theology"<<endl;
     break;//directs Compiler to stop execution and jump to the next case
 case 3:
-       cout << "Monday Classes\n.................\n" ;
+       cout << "Wednesday Classes\n.................\n" ;
        cout << "\t1. BBIT1010: Physical electronics 2"<<endl;
        cout << "\t2. BBIT1015: Fundamentals of biology"<<endl;
-       cout << "\t1. BBIT1110: Theology"<<endl;
+       cout << "\t3. BBIT1110: Discrete Mathematics"<<endl;
     break;//directs Compiler to stop execution and jump to the next case
 case 4:
-       cout << "Monday Classes\n.................\n" ;
+       cout << "Thursday Classes\n.................\n" ;
        cout << "\t1. BBIT1009: Physical electronics"<<endl;
        cout << "\t2. BBIT1108: Fundamentals of computers"<<endl;
-       cout << "\t1. BBIT1120: Ideology"<<endl;
+       cout << "\t3. BBIT1120: Ideology"<<endl;
     break;//directs Compiler to stop execution and jump to the next case
 case 5:
-       cout << "Monday Classes\n.................\n" ;
+       cout << "Friday Classes\n.................\n" ;
        cout << "\t1. BBIT1112: Maths for engineering"<<endl;
        cout << "\t2. BBIT1104: Physics"<<endl;
-       cout << "\t1. BBIT1119: Masters Theology"<<endl;
+       cout << "\t3. BBIT1119: Masters Theology"<<endl;
     break;//directs Compiler to stop execution and jump to the next case
 default :
-       cout << "No other lesson"<<endl;
+       cout << "No other lesson, Not picked appropraite Day (1-5)"<<endl;
 }
 
 
